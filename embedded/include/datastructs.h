9f498e1fa787cd435330b5da2cd9b434b76885d0
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2022-11-18 11:36:36 +0100

    Add init config code (WIP)

--- embedded/include/datastructs.h
+++ embedded/include/datastructs.h
@@ -13,8 +13,8 @@ typedef struct {
     char* name;                 // Human readable name
     sensor_type type;
     sensor_val_type val_type;
+    uint8_t update_rate;
     unsigned char val[4];       // float* f = (float*) &val;
-    
 
 } sensor_t;
 
@@ -23,7 +23,7 @@ typedef struct {
     uint32_t addr;
     char* name;
     sensor_t sensors[MAX_SLAVE_SENSORS_N];
-    uint32_t timeout;
+    uint32_t keepalive_period;
 
 } slave_t;
 
--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <LittleFS.h>
+#include <ArduinoJson.h>
+
+char* configFile = "config.json";     // Config JSON filename (stored in FS)
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void initNode() {
+
+  if(!LittleFS.begin()){
+    Serial.println("error: could not init LittleFS");
+    return;
+  }
+
+  if (!LittleFS.exists(configFile)) {
+    Serial.printf("error: config file ('%s') does not exist\n", configFile);
+    return;
+  }
+
+  File f = LittleFS.open(configFile, "r");
+  if (!f) {
+    Serial.printf("error: could not open config file ()\n", configFile);
+  }
+
+  StaticJsonDocument<512> configJson;
+  deserializeJson(configJson, f.readString());
+
+  Serial.println((const char*) configJson["name"]);
+
 }
