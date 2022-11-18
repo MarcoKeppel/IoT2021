7aa58b590b6dc0e5d7a609d397418942392bdbb7
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2022-11-18 10:38:43 +0100

    Add data structs file (WIP)

---
+++ embedded/include/datastructs.h
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+/*
+    Master
+*/
+
+#define MAX_SLAVES 8
+#define MAX_SLAVE_SENSORS_N 8
+
+
+typedef struct {
+
+    char* name;                 // Human readable name
+    sensor_type type;
+    sensor_val_type val_type;
+    unsigned char val[4];       // float* f = (float*) &val;
+    
+
+} sensor_t;
+
+typedef struct {
+
+    uint32_t addr;
+    char* name;
+    sensor_t sensors[MAX_SLAVE_SENSORS_N];
+    uint32_t timeout;
+
+} slave_t;
+
+enum sensor_type { analog, digital, i2c };
+enum sensor_val_type { v_int, v_uint, v_real, v_bool };
