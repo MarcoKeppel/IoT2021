eb2c57d1ceedba9cbc0966b8ffb5d0f92d0afbb4
Lorenzo Orsingher <lorenzo.orsingher@studenti.unitn.it>
2023-01-18 15:58:00 +0100

    resolve conflicts

--- embedded/include/roles.h
+++ embedded/include/roles.h
@@ -9,90 +9,199 @@
 #include "datastructs.h"
 #include "states.h"
 #include "config.h"
+#include <painlessMesh.h>
+#include <ArduinoJson.h>
 
+typedef struct slave_data
+{
 
-typedef struct {
-
-    const char* name;
+    const char *name;
     uint32_t masterAddr;
-    sensor_t sensors[MAX_SLAVE_SENSORS_N];      // Static size array instead? With size defined by MAX_SLAVE_SENSORS_N macro
+    sensor_t sensors[MAX_SLAVE_SENSORS_N]; // Static size array instead? With size defined by MAX_SLAVE_SENSORS_N macro
     int sensors_n = 0;
 
     uint8_t state;
+    uint32_t currentMillis = 0;
+    uint32_t lastSensorsUpdateMillis = 0;
+    uint32_t lastBroadcastMillis = 0;
 
-    void loadConfig() {
+    painlessMesh *mesh;
 
-        if(!LittleFS.begin()){
+    slave_data(painlessMesh *mesh)
+    {
+        this->mesh = mesh;
+    }
+
+    void slaveLoop()
+    {
+
+        switch (state)
+        {
+
+        case SS_MASTER_REQ:
+            if (currentMillis >= lastBroadcastMillis + BROADCAST_PERIOD)
+            {
+                lastBroadcastMillis = currentMillis;
+                sendMasterAddrReq();
+            }
+            break;
+
+        case SS_SENS_ADV:
+            // sendSensorListAdv();
+            break;
+
+        case SS_SENS_UPD:
+            if (currentMillis > lastSensorsUpdateMillis + SENSORS_UPDATE_PERIOD)
+            {
+                // sendSensorValUpdate();
+            }
+            break;
+        }
+    }
+
+    void sendMasterAddrReq()
+    {
+
+        StaticJsonDocument<512> msg; // TODO: define size as macro
+
+        msg["id"] = mesh->getNodeId();
+        msg["type"] = 0; // TODO: define types more formally
+
+        char msgSerialized[256]; // TODO: define size as macro
+        serializeJson(msg, msgSerialized);
+        mesh->sendBroadcast(msgSerialized, true);
+    }
+
+    // void sendSensorListAdv()
+    // {
+    // }
+
+    // void sendSensorValUpdate()
+    // {
+
+    //     lastSensorsUpdateMillis = currentMillis;
+
+    //     // Create JSON doc...
+    //     StaticJsonDocument<512> msg; // TODO: define size as macro
+    //     msg["id"] = mesh.getNodeId();
+    //     msg["type"] = 9; // TODO: define types more formally
+    //     JsonArray sensorsArray = msg.createNestedArray("sensors");
+
+    //     // ...then cycle through all sensors and add those that need to be updated
+    //     for (int i = 0; i < sensors_n; i++)
+    //     {
+
+    //         // 1 period has passed!
+    //         sensors_update_rate[i]--;
+
+    //         if (sensors_update_rate[i] <= 0)
+    //         {
+
+    //             sensors_update_rate[i] = sensors[i].update_rate;
+
+    //             // Read sensor value, method should depend on val_type
+    //             // For now assume it's always digitalRead()
+    //             sensors[i].val = digitalRead(sensors[i].pin);
+
+    //             // Serial.printf("sensors[%d].val: %d\n", i, sensors[i].val);
+
+    //             // Add data to message
+    //             JsonObject sensorObject = sensorsArray.createNestedObject();
+    //             sensorObject["index"] = i;
+    //             sensorObject["val"] = sensors[i].val;
+    //         }
+    //     }
+
+    //     // If some sensors have been updated, send message
+    //     if (!(sensorsArray.size() <= 0))
+    //     {
+
+    //         char msgSerialized[256]; // TODO: define size as macro
+    //         // String msgSerialized;
+    //         serializeJson(msg, msgSerialized);
+    //         mesh.sendSingle(slaveData.masterAddr, msgSerialized);
+    //     }
+    // }
+
+    void loadConfig()
+    {
+
+        if (!LittleFS.begin())
+        {
             Serial.println("error: could not init LittleFS");
             return;
         }
 
-        if (!LittleFS.exists(configFile)) {
+        if (!LittleFS.exists(configFile))
+        {
             Serial.printf("error: config file ('%s') does not exist\n", configFile);
             return;
         }
 
         File f = LittleFS.open(configFile, "r");
-        if (!f) {
+        if (!f)
+        {
             Serial.printf("error: could not open config file (%s)\n", configFile);
         }
 
         StaticJsonDocument<STATIC_JSON_DOC_SIZE> configJson;
         deserializeJson(configJson, f.readString());
 
-        Serial.println((const char*) configJson["name"]);
-        this->name = (const char*) configJson["name"];
+        Serial.println((const char *)configJson["name"]);
+        this->name = (const char *)configJson["name"];
 
         initSensors(configJson);
 
         printSensors();
-
     }
 
-    void initSensors(StaticJsonDocument<STATIC_JSON_DOC_SIZE> configJson) {
+    void initSensors(StaticJsonDocument<STATIC_JSON_DOC_SIZE> configJson)
+    {
 
         JsonArray sensorsJson = configJson["sensors"].as<JsonArray>();
 
-        for (JsonObject s : sensorsJson) {
+        for (JsonObject s : sensorsJson)
+        {
 
-            if (sensors_n >= MAX_SLAVE_SENSORS_N) break;
+            if (sensors_n >= MAX_SLAVE_SENSORS_N)
+                break;
 
             // TODO: this should really be its own function
-            
+
             // Serial.printf("initSensors(): '%s'\n", (const char*) s["name"]);
-            sensors[sensors_n].name        =       (const char*) s["name"];
-            sensors[sensors_n].type        =     (sensor_type_t) s["type"];
-            sensors[sensors_n].val_type    = (sensor_val_type_t) s["val_type"];
-            sensors[sensors_n].update_rate =           (uint8_t) s["update_rate"];
-            sensors[sensors_n].pin         =           (uint8_t) s["pin"];
+            sensors[sensors_n].name = (const char *)s["name"];
+            sensors[sensors_n].type = (sensor_type_t)s["type"];
+            sensors[sensors_n].val_type = (sensor_val_type_t)s["val_type"];
+            sensors[sensors_n].update_rate = (uint8_t)s["update_rate"];
+            sensors[sensors_n].pin = (uint8_t)s["pin"];
             sensors_n++;
         }
     }
 
-    void printSensors() {
+    void printSensors()
+    {
 
-        for (int i = 0; i < sensors_n; i++) {
+        for (int i = 0; i < sensors_n; i++)
+        {
 
             Serial.printf(
-            "{\n\tname: %s\n\ttype: %u\n\tval_type: %u\n\tupdate_rate: %u\n\tpin: %u\n},\n",
-            sensors[i].name,
-            sensors[i].type,
-            sensors[i].val_type,
-            sensors[i].update_rate,
-            sensors[i].pin
-            );
+                "{\n\tname: %s\n\ttype: %u\n\tval_type: %u\n\tupdate_rate: %u\n\tpin: %u\n},\n",
+                sensors[i].name,
+                sensors[i].type,
+                sensors[i].val_type,
+                sensors[i].update_rate,
+                sensors[i].pin);
         }
     }
-
 } slave_data_t;
 
-typedef struct {
-    
-    char* name;
-    slave_t* slaves;        // Static size array instead? With size defined by MAX_SLAVES_N macro
+typedef struct master_data
+{
+
+    char *name;
+    slave_t *slaves; // Static size array instead? With size defined by MAX_SLAVES_N macro
 
     uint8_t state;
-
 } master_data_t;
 
 #endif
--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -31,20 +31,18 @@ void slaveLoop();
 void printSensors();
 
 uint32_t currentMillis = 0;
+uint32_t lastSensorsUpdateMillis = 0;
+uint32_t lastBroadcastMillis = 0;
 
 painlessMesh mesh;
 
-slave_data_t slaveData;
+// slave_data_t slaveData;
 
 sensor_t sensors[MAX_SLAVE_SENSORS_N];
 int16_t sensors_update_rate[MAX_SLAVE_SENSORS_N] = {0};
 int sensors_n = 0;
 
-uint32_t lastSensorsUpdateMillis = 0;
-
-uint8_t state = 0;      // TODO: handle states with better state machine implementation (e.g. states enum)
-
-uint32_t lastBroadcastMillis = 0;
+uint8_t state = 0; // TODO: handle states with better state machine implementation (e.g. states enum)
 
 void sendMasterAddrReq();
 void sendSensorValUpdate();
@@ -74,111 +72,21 @@ void loop()
   currentMillis = millis();
 
   // masterLoop();
-  slaveLoop();
+  // slaveLoop();
 
   // Run mesh update
   mesh.update();
 }
 
-void initMesh() {
+void initMesh()
+{
 
   mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
   mesh.onReceive(&onReceive);
 }
 
-void sendMasterAddrReq()
-{
-
-  StaticJsonDocument<512> msg; // TODO: define size as macro
-
-  msg["id"] = mesh.getNodeId();
-  msg["type"] = 0; // TODO: define types more formally
-
-  char msgSerialized[256]; // TODO: define size as macro
-  serializeJson(msg, msgSerialized);
-  mesh.sendBroadcast(msgSerialized, true);
-}
-
-void sendSensorValUpdate()
-{
-
-  lastSensorsUpdateMillis = currentMillis;
-
-  // Create JSON doc...
-  StaticJsonDocument<512> msg; // TODO: define size as macro
-  msg["id"] = mesh.getNodeId();
-  msg["type"] = 9; // TODO: define types more formally
-  JsonArray sensorsArray = msg.createNestedArray("sensors");
-
-  // ...then cycle through all sensors and add those that need to be updated
-  for (int i = 0; i < sensors_n; i++)
-  {
-
-    // 1 period has passed!
-    sensors_update_rate[i]--;
-
-    if (sensors_update_rate[i] <= 0)
-    {
-
-      sensors_update_rate[i] = sensors[i].update_rate;
-
-      // Read sensor value, method should depend on val_type
-      // For now assume it's always digitalRead()
-      sensors[i].val = digitalRead(sensors[i].pin);
-
-      // Serial.printf("sensors[%d].val: %d\n", i, sensors[i].val);
-
-      // Add data to message
-      JsonObject sensorObject = sensorsArray.createNestedObject();
-      sensorObject["index"] = i;
-      sensorObject["val"] = sensors[i].val;
-    }
-  }
-
-  // If some sensors have been updated, send message
-  if (!(sensorsArray.size() <= 0))
-  {
-
-    char msgSerialized[256]; // TODO: define size as macro
-    // String msgSerialized;
-    serializeJson(msg, msgSerialized);
-    mesh.sendSingle(slaveData.masterAddr, msgSerialized);
-  }
-}
-
-void sendSensorListAdv()
-{
-}
-
 void masterLoop()
 {
-}
-
-void slaveLoop()
-{
-
-  switch (state)
-  {
-
-  case SS_MASTER_REQ:
-    if (currentMillis >= lastBroadcastMillis + BROADCAST_PERIOD)
-    {
-      lastBroadcastMillis = currentMillis;
-      sendMasterAddrReq();
-    }
-    break;
-
-  case SS_SENS_ADV:
-    sendSensorListAdv();
-    break;
-
-  case SS_SENS_UPD:
-    if (currentMillis > lastSensorsUpdateMillis + SENSORS_UPDATE_PERIOD)
-    {
-      sendSensorValUpdate();
-    }
-    break;
-  }
 }
 
 void onReceive(uint32_t from, const String &msg)
@@ -213,7 +121,7 @@ void onReceiveSlave(uint32_t from, const JsonDocument &msg)
   {
 
   case 1:
-    slaveData.masterAddr = from;
+    // slaveData.masterAddr = from;
     state = 1;
     break;
   }
