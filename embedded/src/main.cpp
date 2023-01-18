557df2d2f7372d413222434dd5addc71cebbf927
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2023-01-18 15:33:11 +0100

    Finalize merge conflicts resolution

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -3,21 +3,8 @@
 #include <painlessMesh.h>
 #include <ArduinoJson.h>
 
-<<<<<<< HEAD
 #include "roles.h"
 
-=======
-#define MESH_SSID "UNITN_IOT"
-#define MESH_PASSWORD "isthissecureenough?"
-#define MESH_PORT 2468
-
-#include "roles.h"
-
-#define STATIC_JSON_DOC_SIZE 512
-
-#define SENSORS_UPDATE_PERIOD 1000 // [ms]
-
->>>>>>> fd980640dc79a96805058b1ec9861da61b0bd46b
 // Pin mapping 'pins_arduino.h'
 /*
   static const uint8_t D0   = 16;
@@ -55,15 +42,8 @@ int sensors_n = 0;
 
 uint32_t lastSensorsUpdateMillis = 0;
 
-<<<<<<< HEAD
 uint8_t state = 0;      // TODO: handle states with better state machine implementation (e.g. states enum)
 
-=======
-const char *configFile = "config.json"; // Config JSON filename (stored in FS)
-
-uint8_t state = 0; // TODO: handle states with better state machine implementation (e.g. states enum)
-#define BROADCAST_PERIOD 2000
->>>>>>> fd980640dc79a96805058b1ec9861da61b0bd46b
 uint32_t lastBroadcastMillis = 0;
 
 void sendMasterAddrReq();
@@ -100,85 +80,7 @@ void loop()
   mesh.update();
 }
 
-<<<<<<< HEAD
 void initMesh() {
-=======
-void initNode()
-{
-
-  if (!LittleFS.begin())
-  {
-    Serial.println("error: could not init LittleFS");
-    return;
-  }
-
-  if (!LittleFS.exists(configFile))
-  {
-    Serial.printf("error: config file ('%s') does not exist\n", configFile);
-    return;
-  }
-
-  File f = LittleFS.open(configFile, "r");
-  if (!f)
-  {
-    Serial.printf("error: could not open config file (%s)\n", configFile);
-  }
-
-  StaticJsonDocument<STATIC_JSON_DOC_SIZE> configJson;
-  deserializeJson(configJson, f.readString());
-
-  Serial.println((const char *)configJson["name"]);
-  slaveData.name = (const char *)configJson["name"];
-
-  initSensors(configJson);
-
-  printSensors();
-}
-
-void initSensors(StaticJsonDocument<STATIC_JSON_DOC_SIZE> configJson)
-{
-
-  JsonArray sensorsJson = configJson["sensors"].as<JsonArray>();
-
-  for (JsonObject s : sensorsJson)
-  {
-
-    if (sensors_n >= MAX_SLAVE_SENSORS_N)
-      break;
-
-    // TODO: this should really be its own function
-
-    // Serial.printf("initSensors(): '%s'\n", (const char*) s["name"]);
-    sensors[sensors_n].name = (const char *)s["name"];
-    sensors[sensors_n].type = (sensor_type_t)s["type"];
-    sensors[sensors_n].val_type = (sensor_val_type_t)s["val_type"];
-    sensors[sensors_n].update_rate = (uint8_t)s["update_rate"];
-    sensors[sensors_n].pin = (uint8_t)s["pin"];
-    sensors_n++;
-  }
-
-  slaveData.sensors = sensors;
-}
-
-void printSensors()
-{
-
-  for (int i = 0; i < sensors_n; i++)
-  {
-
-    Serial.printf(
-        "{\n\tname: %s\n\ttype: %u\n\tval_type: %u\n\tupdate_rate: %u\n\tpin: %u\n},\n",
-        sensors[i].name,
-        sensors[i].type,
-        sensors[i].val_type,
-        sensors[i].update_rate,
-        sensors[i].pin);
-  }
-}
-
-void initMesh()
-{
->>>>>>> fd980640dc79a96805058b1ec9861da61b0bd46b
 
   mesh.init(MESH_SSID, MESH_PASSWORD, MESH_PORT);
   mesh.onReceive(&onReceive);
