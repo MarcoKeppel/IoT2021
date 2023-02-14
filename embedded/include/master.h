3f065795d4ca962d35add646b602594ca57e4b50
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2023-02-14 17:48:33 +0100

    Add TODO

--- embedded/include/master.h
+++ embedded/include/master.h
@@ -13,6 +13,9 @@
 #include <painlessMesh.h>
 #include <ArduinoJson.h>
 
+
+// TODO: even after sensor advertisement reply is sent, sensor data should not be read as it will not be valid (either 0 or old data from another slave)
+
 typedef struct master_data
 {
 
@@ -219,6 +222,7 @@ typedef struct master_data
 
     void updateSensorValues(uint32_t addr, const JsonDocument &msg)
     {
+
         int32_t s = findSlave(addr);
         if (s < 0)
             return;
