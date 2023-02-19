631014c3feb03a25691e9d3d5d98008447614fad
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2023-02-19 18:53:45 +0100

    Add GUI prototype

--- redacted/redacted.py
+++ redacted/redacted.py
@@ -1,19 +1,51 @@
+#from gevent import monkey
+#monkey.patch_all()
+
+
+
 import serial as pyserial
 import json
-import os
+import threading
 from datastructs import *
+
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+# frm = ttk.Frame(root, padding=10)
+# label_frame = ttk.LabelFrame(frm, text = "LABELFRAME WIDGET").grid(column=0, row=0)
+# frm.grid()
+
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+# create a LabelFrame inside the frame
+lblfrm = ttk.LabelFrame(frm, text='My LabelFrame')
+lblfrm.grid()
+
+# add some content to the LabelFrame
+lbl = ttk.Label(lblfrm, text='This is my LabelFrame')
+lbl.grid()
+
+# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 
 
 serial_port = "COM6"
 serial_speed = 115200
 
-if __name__ == "__main__":
+def serial_thread():
+
+    print("SERIAL STARTING")
 
     serial = pyserial.Serial(serial_port, serial_speed)
 
     serial.write('rst\n'.encode())
 
     slaves = { }
+    labels = { }
 
     while True:
 
@@ -31,6 +63,15 @@ if __name__ == "__main__":
         elif slave_msg["type"] == 2:
             if msg["from"] in slaves:
                 slaves[msg["from"]].set_sensors(slave_msg["sensors"])
+                label_txt = ""
+                label_txt = ''
+                label_txt += 'name: {}\n'.format(slaves[msg["from"]].name)
+                label_txt += '\tsensors:\n'
+                for i in slaves[msg["from"]].sensors:
+                    label_txt += '\tname: {}\n'.format(i.name)
+                    label_txt += '\t\tvalue {}\n'.format(i.val)
+                labels[msg["from"]] = ttk.Label( root, text = label_txt.strip())
+                labels[msg["from"]].grid(column=(len(labels) % 2), row=(int(len(labels) / 2)), padx=10, pady=10)
                 print("\n\n\n\n\n\n\n\n")
                 for s in slaves:
                     print(s)
@@ -39,6 +80,14 @@ if __name__ == "__main__":
             if msg["from"] in slaves:
                 for s in slave_msg["sensors"]:
                     slaves[msg["from"]].sensors[s["index"]].val = s["val"]
+                label_txt = ""
+                label_txt = ''
+                label_txt += 'name: {}\n'.format(slaves[msg["from"]].name)
+                label_txt += '\tsensors:\n'
+                for i in slaves[msg["from"]].sensors:
+                    label_txt += '\tname: {}\n'.format(i.name)
+                    label_txt += '\t\tvalue {}\n'.format(i.val)
+                labels[msg["from"]].config(text=label_txt)
                 print("\n\n\n\n\n\n\n\n")
                 for k in slaves:
                     slave = slaves[k]
@@ -50,3 +99,10 @@ if __name__ == "__main__":
                         s += '\t\tvalue {}\n'.format(i.val)
                     print(s.strip())
 
+
+if __name__ == "__main__":
+
+    ts = threading.Thread(target=serial_thread)
+    ts.start()
+
+    root.mainloop()
