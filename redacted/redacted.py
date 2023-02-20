913d45d76970abb097252bd3a33729c82d890bfc
Marco Keppel <33182847+MarcoKeppel@users.noreply.github.com>
2023-02-20 21:30:36 +0100

    Revert back to not using textual framework 💀

--- redacted/redacted.py
+++ redacted/redacted.py
@@ -6,94 +6,103 @@
 import serial as pyserial
 import json
 import threading
-import time
 from datastructs import *
 
 
-import gui
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+# frm = ttk.Frame(root, padding=10)
+# label_frame = ttk.LabelFrame(frm, text = "LABELFRAME WIDGET").grid(column=0, row=0)
+# frm.grid()
+
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+# create a LabelFrame inside the frame
+lblfrm = ttk.LabelFrame(frm, text='My LabelFrame')
+lblfrm.grid()
+
+# add some content to the LabelFrame
+lbl = ttk.Label(lblfrm, text='This is my LabelFrame')
+lbl.grid()
+
+# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 
 
 serial_port = "COM6"
 serial_speed = 115200
 
-event = threading.Event()
-
-app = gui.SlavesManager()
-
 def serial_thread():
-
-    global app
 
     print("SERIAL STARTING")
 
-    # serial = pyserial.Serial()
-    # serial.port = serial_port
-    # serial.baudrate = serial_speed
-    # # serial.timeout = 1
-    # serial.setDTR(False)
-    # serial.open()
     serial = pyserial.Serial(serial_port, serial_speed)
 
-    time.sleep(1)
-
-    serial.reset_input_buffer()
-
-    serial.write('upd\n'.encode())
+    serial.write('rst\n'.encode())
 
     slaves = { }
+    labels = { }
 
     while True:
-
-        if event.is_set():
-            serial.close()
-            break
-
-        if not serial.in_waiting:
-            time.sleep(0.01)
-            continue
 
         str_msg = serial.readline()
         if '{'.encode() not in str_msg:
             continue
         
         msg = json.loads(str_msg.decode())
-        # slave_msg = msg["msg"]
-        #print(msg)
-        app.send_msg(msg)
+        slave_msg = msg["msg"]
         #print(msg)
         
-        # if slave_msg["type"] == 0:
-        #     slaves[msg["from"]] = Slave(msg["from"], slave_msg["name"])
+        if slave_msg["type"] == 0:
+            slaves[msg["from"]] = Slave(msg["from"], slave_msg["name"])
 
-        # elif slave_msg["type"] == 2:
-        #     if msg["from"] in slaves:
-        #         slaves[msg["from"]].set_sensors(slave_msg["sensors"])
-        #         print("\n\n\n\n\n\n\n\n")
-        #         for s in slaves:
-        #             print(s)
+        elif slave_msg["type"] == 2:
+            if msg["from"] in slaves:
+                slaves[msg["from"]].set_sensors(slave_msg["sensors"])
+                label_txt = ""
+                label_txt = ''
+                label_txt += 'name: {}\n'.format(slaves[msg["from"]].name)
+                label_txt += '\tsensors:\n'
+                for i in slaves[msg["from"]].sensors:
+                    label_txt += '\tname: {}\n'.format(i.name)
+                    label_txt += '\t\tvalue {}\n'.format(i.val)
+                labels[msg["from"]] = ttk.Label( root, text = label_txt.strip())
+                labels[msg["from"]].grid(column=(len(labels) % 2), row=(int(len(labels) / 2)), padx=10, pady=10)
+                print("\n\n\n\n\n\n\n\n")
+                for s in slaves:
+                    print(s)
         
-        # elif slave_msg["type"] == 9:
-        #     if msg["from"] in slaves:
-        #         for s in slave_msg["sensors"]:
-        #             slaves[msg["from"]].sensors[s["index"]].val = s["val"]
-        #         print("\n\n\n\n\n\n\n\n")
-        #         for k in slaves:
-        #             slave = slaves[k]
-        #             s = ''
-        #             s += 'name: {}\n'.format(slave.name)
-        #             s += '\tsensors:\n'
-        #             for i in slave.sensors:
-        #                 s += '\tname: {}\n'.format(i.name)
-        #                 s += '\t\tvalue {}\n'.format(i.val)
-        #             print(s.strip())
+        elif slave_msg["type"] == 9:
+            if msg["from"] in slaves:
+                for s in slave_msg["sensors"]:
+                    slaves[msg["from"]].sensors[s["index"]].val = s["val"]
+                label_txt = ""
+                label_txt = ''
+                label_txt += 'name: {}\n'.format(slaves[msg["from"]].name)
+                label_txt += '\tsensors:\n'
+                for i in slaves[msg["from"]].sensors:
+                    label_txt += '\tname: {}\n'.format(i.name)
+                    label_txt += '\t\tvalue {}\n'.format(i.val)
+                labels[msg["from"]].config(text=label_txt)
+                print("\n\n\n\n\n\n\n\n")
+                for k in slaves:
+                    slave = slaves[k]
+                    s = ''
+                    s += 'name: {}\n'.format(slave.name)
+                    s += '\tsensors:\n'
+                    for i in slave.sensors:
+                        s += '\tname: {}\n'.format(i.name)
+                        s += '\t\tvalue {}\n'.format(i.val)
+                    print(s.strip())
 
-
-#slaves = 
 
 if __name__ == "__main__":
 
-    ts = threading.Thread(target=serial_thread, args=[ ])
+    ts = threading.Thread(target=serial_thread)
     ts.start()
-    app.run()
-    event.set()
-    ts.join()
+
+    root.mainloop()
